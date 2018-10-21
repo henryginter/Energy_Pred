9a62f9c9977c9e4b4a73b409fc2b9ba7dfbdd1e4
henryginter <henry.ginter@protonmail.com>
2018-10-21 19:52:30 +0300

    simplified code

--- buildings.py
+++ buildings.py
@@ -1,49 +1,115 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from mpl_toolkits.axes_grid1 import host_subplot
+import mpl_toolkits.axisartist as AA
 
-# Importing the dataset
+# Importing the datasets
 df1 = pd.read_csv('building el consumption data 1.csv', sep=';')
 df2 = pd.read_csv('building el consumption data 2.csv', sep=';')
-remove_list = [2818, 7815, 7821]
+remove_list = [2818, 7815, 7821, 5988] # Removing duplicate/empty sensor data
 for i in remove_list:
     df2 = df2[df2['reta_id'] != i]
-df = pd.concat([df1, df2]).drop_duplicates(subset=['reta_id', 'READ_TIME'])
+df = pd.concat([df1, df2]) # Joining dataframes together to reformat the data
 
-#dtest = dataset[dataset.index>38073]
-
+# Creating separate columns for sensors
 onehot = pd.get_dummies(df['reta_id']) 
 df = df.drop(['reta_id'], axis =1)
-df = df.join(onehot)
+df = pd.concat([df, onehot], axis=1)
 
+# Assigning tag values to correct sensor columns
 col = list(onehot)
 for i in col:
-    df[i] = np.where(df[i]==1, df['TAG_VALUE'], 0)
-df = df[df[7821] <= 100]
-df = df.drop(['CAT_VAR', 'TAG_VALUE'], axis = 1)
-df = df.groupby('READ_TIME').sum()
-mbep1_col = col[8:12]
-mbep2_col = col[0:6]
-df1 = df.drop(mbep2_col, axis = 1)
+    df[i] = np.where(df[i]==1, df['tag_value'], np.NaN)
+
+mbep1_col = col[9:13] # Building #1 power consumption columns
+mbep2_col = col[0:6] # Building #2 power consumption columns
+sen_col = list(set(col) - set(mbep1_col+mbep2_col)) # Sensor columns
+df = df.drop(['cat_var', 'tag_value'], axis = 1) # Removing unnecessary columns
+df = df.groupby('read_time').sum() # Combining rows with same timestamp
+
+# Separating data into different dataframes to clear empty values
+df1 = df[mbep1_col]
 df2 = df[mbep2_col]
+df3 = df[sen_col]
 for i in mbep1_col:
     df1 = df1[df1[i] != 0]
 for i in mbep2_col:
     df2 = df2[df2[i] != 0]
-df = pd.concat([df1, df2]).drop_duplicates()
-df['mbep_sum1'] = df[mbep1_col].sum(axis = 1)
-df['mbep_sum2'] = df[mbep2_col].sum(axis = 1)
+    
+# Removing anomalous humidity readings
+mask = df3[7821] > 100.06
+df3.loc[mask, 7821] = np.NaN
 
-dtest.index = pd.to_datetime(dtest.index)
-dtestX = dtest.resample('H').mean()
+# Summing up power meters
+df1['mbep_sum1'] = df1[mbep1_col].sum(axis = 1)
+df2['mbep_sum2'] = df2[mbep2_col].sum(axis = 1)
 
-plt.plot(dtestX.index, dtestX[7815], 'r', label ='Solar radiation')
-plt.plot(dtestX.index, dtestX[7821], 'pink', label ='Humidity')
-plt.plot(dtestX.index, dtestX['mbep_sum'], 'b', label ='Energy con.')
-plt.plot(dtestZ.index, dtestZ['mbep_sum'], 'indigo', label ='Energy con2')
+# Joining sensor data back to power data and taking hourly means
+df1 = pd.concat([df1, df3], axis=1, join_axes=[df1.index])
+df1.index = pd.to_datetime(df1.index)
+df1 = df1.resample('H').mean()
+df2 = pd.concat([df2, df3], axis=1, join_axes=[df2.index])
+df2.index = pd.to_datetime(df2.index)
+df2 = df2.resample('H').mean()
+
+# For creating models of df1 or df2, continue to mudel.py. For creating plots, continue below
+
+
+
+# Joining dataframes together for plotting
+dataset = pd.DataFrame()
+dataset = dataset.join(df1['mbep_sum1'], how='outer')
+dataset = dataset.join(df2['mbep_sum2'], how='outer')
+dataset = dataset.join(df3, how='outer')
+dataset.index = pd.to_datetime(dataset.index)
+dtestX = dataset.resample('H').mean()
+
+# Find correlation value cor and create correlation plot
+from numpy.polynomial.polynomial import polyfit
+col = ['mbep_sum1', 'mbep_sum2']
+for i in col:
+    dtestX = dtestX[pd.notnull(dtestX[i])] 
+x = dtestX['mbep_sum1']
+y = dtestX['mbep_sum2']
+b, m = polyfit(x, y, 1)
+cor = np.corrcoef(x, y)
+plt.style.use('ggplot')
+plt.scatter(x, y)
+plt.plot(x, b + m * x, 'blue')
+plt.xlabel('Building 1 consumption (kW)')
+plt.ylabel('Building 2 consumption (kW)')
 plt.show()
 
-dtest.to_csv('1_H.csv')
+# Create unified plot for general analysis
+plt.style.use('ggplot')
+plt.plot(dtestX.index, dtestX[7815], 'red', label ='Solar radiation')
+plt.plot(dtestX.index, dtestX[7821], 'gray', label ='Humidity')
+plt.plot(dtestX.index, dtestX['mbep_sum1'], 'blue', label ='Energy con.')
+plt.plot(dtestX.index, dtestX['mbep_sum2'], 'indigo', label ='Energy con2')
+plt.plot(dtestX.index, dtestX[5988], 'orange', label ='Ambient Temperature')
+plt.legend(loc='upper left')
+plt.show()
 
-compare = pd.DataFrame()
-compare
+# Energy consumption and ambient temperature plot with separate y-axises
+plt.style.use('ggplot')
+from matplotlib import rc
+rc('mathtext', default='regular')
+fig = plt.figure()
+ax = fig.add_subplot(111)
+lns1 = ax.plot(dtestX.index, dtestX['mbep_sum1'], 'blue', label = 'Building #1')
+lns2 = ax.plot(dtestX.index, dtestX['mbep_sum2'], 'indigo', label = 'Building #2')
+ax2 = ax.twinx()
+lns3 = ax2.plot(dtestX.index, dtestX[5988], '-r', label = 'Ambient temperature')
+lns = lns1+lns2+lns3
+labs = [l.get_label() for l in lns]
+ax.legend(lns, labs, loc=0)
+ax.grid()
+ax.set_xlabel("Time (week)")
+ax.set_ylabel(r"Average weekly energy consumption (kW)")
+ax2.set_ylabel(r"Temperature ($^\circ$C)")
+ax2.set_ylim(0, 35)
+ax.set_ylim(0,1000)
+plt.show()
+
+
--- mudel.py
+++ mudel.py
@@ -1,23 +1,91 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.metrics import mean_squared_error
+from math import sqrt
 
-# Importing the dataset
+# Importing the dataset from buildings.py
 dataset = df1
 dataset.index = pd.to_datetime(dataset.index)
 dataset = dataset.resample('H').mean()
 
+# Creating inputs form timestamps
 dataset['M'] = dataset.index.month
 dataset['D'] = dataset.index.weekday
 dataset['H'] = dataset.index.hour
+dataset['e24'] = dataset['mbep_sum1'].shift(24) # Creating an input from power consumption 24h ago
 
+# Taking daily means from sensor data, shifting them 24h and filling empty rows
+dataset['t_day'] = dataset[5988].resample('D').mean()
+dataset['hum_day'] = dataset[7821].resample('D').mean()
+dataset['sr_day'] = dataset[7815].resample('D').mean()
+dataset['t_day'] = dataset['t_day'].fillna(method='ffill').shift(24)
+dataset['hum_day'] = dataset['hum_day'].fillna(method='ffill').shift(24)
+dataset['sr_day'] = dataset['sr_day'].fillna(method='ffill').shift(24)
 
+# Create work_day category for building number 2 and shift e24 for additional 48 hours on Mondays
+#dataset['work_day'] = 1
+#mask = dataset['D'] > 4
+#dataset.loc[mask, 'work_day'] = 0
+#mask = dataset['D'] == 0
+#dataset.loc[mask, 'e24'] = dataset['e24'].shift(48)
 
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+# Removing rows with nan values
+col = ['e24', 't_day', 'hum_day', 'sr_day', 7821]
+for i in col:
+    dataset = dataset[pd.notnull(dataset[i])] 
 
-# Taking care of missing data
-from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
-imputer = imputer.fit(X[:, 1:3])
-X[:, 1:3] = imputer.transform(X[:, 1:3])
+# Defining dependent and independent values (differs for building #2)
+X = dataset.iloc[:, 8:15].values
+y = dataset.iloc[:, 4].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Importing the Keras libraries and packages
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Initialising the ANN
+regressor = Sequential()
+
+# Adding the input layer and the hidden layer
+regressor.add(Dense(output_dim = 7, activation = 'relu', input_dim = 7))
+
+# Adding the output layer
+regressor.add(Dense(output_dim = 1, activation = 'linear'))
+
+# Compiling the ANN
+regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
+
+# Fitting the ANN to the Training set
+regressor.fit(X_train, y_train, batch_size = 10, nb_epoch = 1000)
+
+# Part 3 - Predicting Test set results
+y_pred = regressor.predict(X_test)
+
+# Fitting Multiple Linear Regression to the Training set (Skip this if you want the scores and plot of ANN)
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+y_pred = regressor.predict(X_test)
+
+# Finding R^2 and RMSE
+from sklearn.metrics import r2_score
+R2 = r2_score(y_test, y_pred)
+rms = sqrt(mean_squared_error(y_test, y_pred))
+
+#Plot predicted and real data
+plt.plot(y_test, color = 'red', label = 'Real data')
+plt.plot(y_pred, color = 'blue', label = 'Predicted data')
+plt.title('Prediction')
+plt.legend()
+plt.show()
